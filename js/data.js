7cfb16f014151dd7d72efeca6f3fe7886edd3ca1
margb13 <marg.b13@hotmail.com>
2017-12-30 00:46:37 -0500

    Añadiendo contenido a data.js e imagenes de la carpeta restaurants

---
+++ js/data.js
@@ -0,0 +1,355 @@
+var restaurant = {
+  'La Choza de la Anaconda': {
+    'description': 'Siente la Selva',
+    'qualification': 4,
+    'location': {
+      'Los olivos': 'Av. Carlos Izaguirre',
+      'Surco': 'Av. Caminos del Inca 1666',
+      'Cercado de Lima': 'Av. Salaverry 3680',
+    },
+    'pagina web': 'http://www.lachozadelaanaconda.com/',
+    'logo': '../assets/images/restaurant/la-choza-anaconda/logo.jpg',
+    'photo': '../assets/images/restaurant/la-choza-anaconda/photo.jpg',
+    'foodType': 'Comida de la Selva',
+    'menu': [{
+        'nombre': 'Chaufa a la Anaconda',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/1.jpg',
+        'precio': 'S/. 25.00',
+        'codigo': 'p-1'
+      },
+      {
+        'nombre': 'Tacacho con Cecina',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/2.jpg',
+        'precio': 'S/. 25.00',
+        'codigo': 'p-2'
+      },
+      {
+        'nombre': 'Juane de Pollo',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/3.jpg',
+        'precio': 'S/. 28.00',
+        'codigo': 'p-3'
+      },
+      {
+        'nombre': 'Tacu Tacu con Cecina',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/4.jpg',
+        'precio': 'S/. 25.00',
+        'codigo': 'p-4'
+      },
+      {
+        'nombre': 'Inchicapi de Gallina',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/5.jpg',
+        'precio': 'S/. 18.00',
+        'codigo': 'p-5'
+      },
+      {
+        'nombre': 'Ceviche de Doncella',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/6.jpg',
+        'precio': 'S/. 25.00',
+        'codigo': 'p-6'
+      },
+      {
+        'nombre': 'Chicharrón de Doncella',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/7.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'p-7'
+      },
+      {
+        'nombre': 'Palometa Frita con Tacacho',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/8.jpg',
+        'precio': 'S/. 28.00',
+        'codigo': 'p-8'
+      },
+      {
+        'nombre': 'Sudado de Doncella',
+        'photo': '../assets/images/restaurant/la-choza-anaconda/9.jpg',
+        'precio': 'S/. 20.00',
+        'codigo': 'p-9'
+      }
+    ]
+  },
+
+  'El Bolivariano': {
+    'description': 'El verdadero sabor de la Antigua Cocina Peruana',
+    'qualification': 5,
+    'location': {
+      'Pueblo Libre': 'Calle Rosa Toledo 291',
+    },
+    'pagina web': 'http://elbolivariano.com/',
+    'logo': '../assets/images/restaurant/bolivariano/logo.jpg',
+    'photo': '../assets/images/restaurant/bolivariano/photo.jpg',
+    'foodType': 'Comida criolla',
+    'menu': [{
+        'nombre': 'Arroz con Pato',
+        'photo': '../assets/images/restaurant/bolivariano/1.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'b-1'
+      },
+      {
+        'nombre': 'Seco de cabrito',
+        'photo': '../assets/images/restaurant/bolivariano/2.jpg',
+        'precio': 'S/. 29.00',
+        'codigo': 'b-2'
+      },
+      {
+        'nombre': 'Lomo saltado',
+        'photo': '../assets/images/restaurant/bolivariano/3.jpg',
+        'precio': 'S/. 40.00',
+        'codigo': 'b-3'
+      },
+      {
+        'nombre': 'Anticucho',
+        'photo': '../assets/images/restaurant/bolivariano/4.jpg',
+        'precio': 'S/. 35.00',
+        'codigo': 'b-4'
+      },
+      {
+        'nombre': 'Ají de Gallina',
+        'photo': '../assets/images/restaurant/bolivariano/5.jpg',
+        'precio': 'S/. 28.00',
+        'codigo': 'b-5'
+      },
+      {
+        'nombre': 'Brochetas de pollo',
+        'photo': '../assets/images/restaurant/bolivariano/6.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'b-6'
+      },
+      {
+        'nombre': 'Arroz con mariscos',
+        'photo': '../assets/images/restaurant/bolivariano/7.jpg',
+        'precio': 'S/. 38.00',
+        'codigo': 'b-7'
+      },
+      {
+        'nombre': 'Lechón al cilindro',
+        'photo': '../assets/images/restaurant/bolivariano/8.jpg',
+        'precio': 'S/. 45.00',
+        'codigo': 'b-8'
+      },
+      {
+        'nombre': 'Chancho a la caja China',
+        'photo': '../assets/images/restaurant/bolivariano/9.jpg',
+        'precio': 'S/. 50.00',
+        'codigo': 'b-9'
+      }
+    ]
+  },
+
+  'Edo Sushi Bar': {
+    'informacion': 'Más de 12 años compartiendo tradición japonesa que engrandece el sabor del Perú',
+    'qualification': 4.4,
+    'location': {
+      'San Borja': 'Av. San Borja Sur 663',
+      'Miraflores': 'Calle Berlín 601',
+      'Magdalena': 'Calle José Cossio 292',
+      'San Miguel': 'Av. Monseñor Jorge Dintilhac 665',
+      'La Molina': 'Jr. Las Caobas 180',
+      'Surco': 'CC. Jockey Plaza CP4-9',
+      'San Isidro': 'Av. Jorge Basadre 275'
+    },
+    'pagina web': 'https://edosushibar.com/',
+    'logo': '../assets/images/restaurant/edo-sushi-bar/logo.jpg',
+    'photo': '../assets/images/restaurant/edo-sushi-bar/photo.jpg',
+    'foodType': 'Comida Japonesa',
+    'menu': [{
+        'nombre': 'Uramaki',
+        'photo': '../assets/images/restaurant/edo-sushi-bar/1.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'e-1'
+      },
+      {
+        'nombre': 'Gunkan',
+        'photo': '../assets/images/restaurant/edo-sushi-bar/2.jpg',
+        'precio': 'S/. 25.00',
+        'codigo': 'e-2'
+      },
+      {
+        'nombre': 'Ebitai',
+        'photo': '../assets/images/restaurant/edo-sushi-bar/3.jpg',
+        'precio': 'S/. 35.00',
+        'codigo': 'e-3'
+      },
+      {
+        'nombre': 'Teriyaki Wings',
+        'photo': '../assets/images/restaurant/edo-sushi-bar/4.jpg',
+        'precio': 'S/. 35.00',
+        'codigo': 'e-4'
+      },
+      {
+        'nombre': 'Nigiri',
+        'photo': '../assets/images/restaurant/edo-sushi-bar/5.jpg',
+        'precio': 'S/. 28.00',
+        'codigo': 'e-5'
+      },
+      {
+        'nombre': 'Yakitori',
+        'photo': '../assets/images/restaurant/edo-sushi-bar/6.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'e-6'
+      },
+    ]
+  },
+
+  'La Muralla': {
+    'informacion': 'Atención todos los días del año con lo mejor de la comida peruana, criolla y pescados y mariscos',
+    'qualification': 4,
+    'location': {
+      'Cercado de Lima': 'Av. Nicolas de Pierola 1018, Alt. Plaza San Martin',
+    },
+    'pagina web': 'http://lamurallarestaurante.com/',
+    'logo': '../assets/images/restaurant/la-muralla/logo.jpg',
+    'photo': '../assets/images/restaurant/la-muralla/photo.jpg',
+    'foodType': 'Comida Criolla',
+    'menu': [{
+        'nombre': 'Chicharron de pescado',
+        'photo': '../assets/images/restaurant/la-muralla/1.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'm-1'
+      },
+      {
+        'nombre': 'Tiradito de pescado',
+        'photo': '../assets/images/restaurant/la-muralla/2.jpg',
+        'precio': 'S/. 25.00',
+        'codigo': 'm-2'
+      },
+      {
+        'nombre': 'Chupe de camarones',
+        'photo': '../assets/images/restaurant/la-muralla/3.jpg',
+        'precio': 'S/. 35.00',
+        'codigo': 'm-3'
+      },
+      {
+        'nombre': 'Tacu tacu con Pulpo a la parilla',
+        'photo': '../assets/images/restaurant/la-muralla/4.jpg',
+        'precio': 'S/. 35.00',
+        'codigo': 'm-4'
+      },
+      {
+        'nombre': 'Arroz con Pato',
+        'photo': '../assets/images/restaurant/la-muralla/5.jpg',
+        'precio': 'S/. 28.00',
+        'codigo': 'm-5'
+      },
+      {
+        'nombre': 'Tacu tacu con mariscos',
+        'photo': '../assets/images/restaurant/la-muralla/6.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'm-6'
+      },
+    ]
+  },
+
+  'El 10': {
+    'informacion': 'La mejor en carnes y vino',
+    'qualification': 5,
+    'location': {
+      'Miraflores': 'Av. Petit Thouars 4599',
+      'Cercado de Lima': 'Centro Comercial Lima Plaza Norte - Tomas valle',
+      'Cercado de Lima': 'Jr. de la Union 364',
+    },
+    'pagina web': 'http://www.el10.pe/index.php',
+    'logo': '../assets/images/restaurant/el-10/logo.jpg',
+    'photo': '../assets/images/restaurant/el-10/photo.jpg',
+    'foodType': 'Comida Criolla',
+    'menu': [{
+        'nombre': 'Ají de gallina',
+        'photo': '../assets/images/restaurant/el-10/1.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'c-1'
+      },
+      {
+        'nombre': 'Asado de tira',
+        'photo': '../assets/images/restaurant/el-10/2.jpg',
+        'precio': 'S/. 40.00',
+        'codigo': 'c-2'
+      },
+      {
+        'nombre': 'Entraña fina',
+        'photo': '../assets/images/restaurant/el-10/3.jpg',
+        'precio': 'S/. 50.00',
+        'codigo': 'c-3'
+      },
+      {
+        'nombre': 'Pulpo a la Gallega',
+        'photo': '../assets/images/restaurant/el-10/4.jpg',
+        'precio': 'S/. 50.00',
+        'codigo': 'c-4'
+      },
+      {
+        'nombre': 'Parrilla triple',
+        'photo': '../assets/images/restaurant/el-10/5.jpg',
+        'precio': 'S/. 28.00',
+        'codigo': 'c-5'
+      },
+      {
+        'nombre': 'Croquetas de pollo y jamon',
+        'photo': '../assets/images/restaurant/el-10/6.jpg',
+        'precio': 'S/. 30.00',
+        'codigo': 'c-6'
+      },
+    ]
+  },
+
+  'Yen Yen': {
+    'informacion': 'El verdadero sabor oriental disfrutalo en familia',
+    'qualification': 5,
+    'location': {
+      'Los Olivos': 'Av. Las Palmeras 4050',
+      'Los Olivos': 'Av. Antunez de Mayolo 1121',
+      'San Martín de Porres': 'Av. Eduardo de habich 266',
+    },
+    'pagina web': 'http://chifayenyen.com/',
+    'logo': '../assets/images/restaurant/yen-yen/logo.jpg',
+    'photo': '../assets/images/restaurant/yen-yen/photo.jpg',
+    'foodType': 'Comida Criolla',
+    'menu': [{
+        'nombre': 'Tortilla de verduras',
+        'photo': '../assets/images/restaurant/yen-yen/1.jpg',
+        'precio': 'S/. 15.00',
+        'codigo': 'y-1'
+      },
+      {
+        'nombre': 'Sopa Siu Kau',
+        'photo': '../assets/images/restaurant/yen-yen/2.jpg',
+        'precio': 'S/. 12.00',
+        'codigo': 'y-2'
+      },
+      {
+        'nombre': 'Chicharrón de Pollo',
+        'photo': '../assets/images/restaurant/yen-yen/3.jpg',
+        'precio': 'S/. 18.00',
+        'codigo': 'y-3'
+      },
+      {
+        'nombre': 'Chaufa especial',
+        'photo': '../assets/images/restaurant/yen-yen/4.jpg',
+        'precio': 'S/. 18.00',
+        'codigo': 'y-4'
+      },
+      {
+        'nombre': 'Cerdo con Tamarindo',
+        'photo': '../assets/images/restaurant/yen-yen/5.jpg',
+        'precio': 'S/. 18.00',
+        'codigo': 'y-5'
+      },
+      {
+        'nombre': 'Tallarín con langostino',
+        'photo': '../assets/images/restaurant/yen-yen/6.jpg',
+        'precio': 'S/. 20.00',
+        'codigo': 'y-6'
+      },
+      {
+        'nombre': 'Kamlú Wantan',
+        'photo': '../assets/images/restaurant/yen-yen/7.jpg',
+        'precio': 'S/. 15.00',
+        'codigo': 'y-7'
+      },
+      {
+        'nombre': 'Aeropuerto',
+        'photo': '../assets/images/restaurant/yen-yen/8.jpg',
+        'precio': 'S/. 18.00',
+        'codigo': 'y-8'
+      }
+    ]
+  },
+};
