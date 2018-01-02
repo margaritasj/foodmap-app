b39e445c216dbbfd98310e8ea045e61fc76386e9
margb13 <marg.b13@hotmail.com>
2018-01-02 18:09:00 -0500

    Añadiendo funcionalidad para que muestros los restaurantes por distrito

--- js/results.js
+++ js/results.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
   /* -------- Mostrar splash por un de tiempo de 5 seg. -------- */
+  var container = $('.photo-restaurant');
+  var bsExample = $('.bs-example2');
+
+
   // Recorriendo el JSON data.js
-  for (var i = 0; i < restaurants.length; i++) {
+  for (var i = 0; i < restaurants.length - 1; i++) {
     var photoRestaurants = restaurants[i].photo;
 
-    $('img').attr("src",photoRestaurants);
+    // Creando los contenedores:
+    var col = $('<div class = "col-xs-12 col-md-8 col-md-offset-2"> </div>');
+    var thumbnail = $('<a class = "thumbnail card-photo" data-src="holder.js/100%x180"> </a>');
+    var img = $('<img class = "" src="" alt="">');
 
+    var imgPhoto = img.attr("src", photoRestaurants);
+    container.append(bsExample);
+    bsExample.append(col);
+    col.append(thumbnail);
+    thumbnail.append(img);
+
+    $('img').addClass('size-photo');
   }
 });
