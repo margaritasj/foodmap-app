09cdc5a2570def5d91280910d490bce3c9bf135d
margb13 <marg.b13@hotmail.com>
2018-01-03 01:33:38 -0500

    corrigiendo direccionamiento

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   /* -------- Mostrar splash por un de tiempo de 5 seg. -------- */
   setTimeout(function() {
-    window.location.href = '../views/homepage.html';
+    window.location.href = '..foodmap-app/views/homepage.html';
   }, 5000);
 
 });
